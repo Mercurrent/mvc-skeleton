f453150a8c828bfdf09a5028aa7b0dd19e877b51
Tatiana Merkulova <tatianamr@ya.ru>
2012-02-05 18:44:36 +0300

    WrongPasswordException exception is written;

---
+++ src/main/java/ru/hh/school/example/WrongPasswordException.java
@@ -0,0 +1,21 @@
+package ru.hh.school.example;
+
+public class WrongPasswordException extends Exception{
+    
+    private final String email;
+    private final String password;
+    
+    public WrongPasswordException(final String email, final String password) {
+        super(password);
+        this.email = email;
+        this.password = password;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+}
