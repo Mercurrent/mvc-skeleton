273a17d024e7ca05df8feca28382bb55b670081d
Tatiana Merkulova <tatianamr@ya.ru>
2012-02-06 03:06:01 +0300

    RequestForRecommendation constructor is written.

--- src/main/java/ru/hh/school/example/RequestForRecommendation.java
+++ src/main/java/ru/hh/school/example/RequestForRecommendation.java
@@ -2,9 +2,20 @@ package ru.hh.school.example;
 
 public class RequestForRecommendation {
     
-    private final User referee;
-    private final User requester;
-    private final User recommendedUser;
+    private final Long refereeId;
+    private final Long requesterId;
+    private final Long recommendedUserId;
+    private final String text;
+    
+    public RequestForRecommendation(final Long refereeId,
+                                    final Long requesterId,
+                                    final Long recommendedUserId,
+                                    final String text) {
+        this.recommendedUserId = recommendedUserId;
+        this.refereeId = refereeId;
+        this.requesterId = requesterId;
+        this.text = text;
+    }
 
 
             
