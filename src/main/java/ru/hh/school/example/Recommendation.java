79d4dd78bbefc52ba01333f447cb05d52889cc47
Tatiana Merkulova <tatianamr@ya.ru>
2012-02-06 03:11:52 +0300

    Valid features is added to Recommendation class.

--- src/main/java/ru/hh/school/example/Recommendation.java
+++ src/main/java/ru/hh/school/example/Recommendation.java
@@ -7,10 +7,12 @@ public class Recommendation extends Entity {
     private final Long refereeId;
     private final Long recommendedUserId;
     private String text;
+    private boolean valid;
 
     public Recommendation(final Long refereeId, final Long recommendedUserId) {
         this.refereeId = refereeId;
         this.recommendedUserId = recommendedUserId;
+        valid = true;
     }
 
     public void setText(final String text) {
@@ -27,6 +29,14 @@ public class Recommendation extends Entity {
     
     public Long getRecommendedUserId() {
         return recommendedUserId;
+    }
+
+    public boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
     }
 
 }
